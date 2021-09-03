f4e921d6cea298e656683b7b1dae37b2e4346899
Wizi8914 <loick.konieczny@gmail.com>
2021-09-03 19:00:11 +0200

    fix bug

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-const loader = document.querySelector('.loader')
-
 window.addEventListener('load', () => {
-
-    loader.classList.add('fondu')
+    document.querySelector('.loader').style.opacity = "0"
+    document.querySelector('.loader').style.width = "0%"
+    document.querySelector('.loader').style.height = "0%"
 })
