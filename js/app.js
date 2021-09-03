cf4c3287096de2c656d501677c337b3ca4db9465
Wizi8914 <loick.konieczny@gmail.com>
2021-09-03 19:13:39 +0200

    add js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
-    document.querySelector('.loader').style.opacity = "0"
-    document.querySelector('.loader').style.width = "0%"
-    document.querySelector('.loader').style.height = "0%"
+    document.querySelector('.loader').classList.add('fondu-out');
+    setTimeout(() => {
+        document.querySelector('.loader').remove()
+    }, 400);
 })
