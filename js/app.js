37e40c441168133114111f934dd5e1d17c9632af
Wizi8914 <loick.konieczny@gmail.com>
2021-10-22 13:16:37 +0200

    js remove line

--- js/app.js
+++ js/app.js
@@ -8,65 +8,11 @@ window.addEventListener("load", () => {
 
 //===============================    COMMAND LIST    =========================================
 
-function musiclist() {
+function list(number) {
 
-    if(document.querySelector(".listcommand:nth-child(1) ul").style.display === "none") {
-        document.querySelector(".listcommand:nth-child(1) ul").style.display = "list-item"
+    if(document.querySelector(`.listcommand:nth-child(${number}) ul`).style.display === "none") {
+        document.querySelector(`.listcommand:nth-child(${number}) ul`).style.display = "list-item"
     } else {
-        document.querySelector(".listcommand:nth-child(1) ul").style.display = "none"
-    }
-}
-
-function diverslist() {
-
-    if(document.querySelector(".listcommand:nth-child(2) ul").style.display === "none") {
-        document.querySelector(".listcommand:nth-child(2) ul").style.display = "list-item"
-    } else {
-        document.querySelector(".listcommand:nth-child(2) ul").style.display = "none"
-    }
-}
-
-function utilitairelist() {
-
-    if(document.querySelector(".listcommand:nth-child(3) ul").style.display === "none") {
-        document.querySelector(".listcommand:nth-child(3) ul").style.display = "list-item"
-    } else {
-        document.querySelector(".listcommand:nth-child(3) ul").style.display = "none"
-    }
-}
-
-function moderationlist() {
-
-    if(document.querySelector(".listcommand:nth-child(4) ul").style.display === "none") {
-        document.querySelector(".listcommand:nth-child(4) ul").style.display = "list-item"
-    } else {
-        document.querySelector(".listcommand:nth-child(4) ul").style.display = "none"
-    }
-}
-
-function nsfwlist() {
-
-    if(document.querySelector(".listcommand:nth-child(5) ul").style.display === "none") {
-        document.querySelector(".listcommand:nth-child(5) ul").style.display = "list-item"
-    } else {
-        document.querySelector(".listcommand:nth-child(5) ul").style.display = "none"
-    }
-}
-
-function interactionlist() {
-
-    if(document.querySelector(".listcommand:nth-child(6) ul").style.display === "none") {
-        document.querySelector(".listcommand:nth-child(6) ul").style.display = "list-item"
-    } else {
-        document.querySelector(".listcommand:nth-child(6) ul").style.display = "none"
-    }
-}
-
-function animelist() {
-
-    if(document.querySelector(".listcommand:nth-child(7) ul").style.display === "none") {
-        document.querySelector(".listcommand:nth-child(7) ul").style.display = "list-item"
-    } else {
-        document.querySelector(".listcommand:nth-child(7) ul").style.display = "none"
+        document.querySelector(`.listcommand:nth-child(${number}) ul`).style.display = "none"
     }
 }
