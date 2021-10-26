08355ee5fbfacddff38fd9e7b93ac282ca8d037a
Wizi8914 <loick.konieczny@gmail.com>
2021-10-26 20:02:29 +0200

    wip

--- js/app.js
+++ js/app.js
@@ -12,9 +12,14 @@ function list(number) {
 
     if(document.querySelector(`.listcommand:nth-child(${number}) ul`).style.display === "none") {
         document.querySelector(`.listcommand:nth-child(${number}) ul`).style.display = "list-item"
-        document.querySelector(`.listcommand:nth-child(${number})>button>.test>i`).className = "fas fa-angle-down"
+        document.querySelector(`.listcommand:nth-child(${number}) button .arrow i`).className = "fas fa-angle-down"
+        document.querySelector(`.listcommand:nth-child(${number}) button .arrow i`).style.paddingLeft = '5.25px'
+        document.querySelector(`.listcommand:nth-child(${number}) button .arrow i`).style.paddingRight = '5.25px'
+
     } else {
         document.querySelector(`.listcommand:nth-child(${number}) ul`).style.display = "none"
-        document.querySelector(`.listcommand:nth-child(${number})>button>.test>i`).className = "fas fa-angle-right"
+        document.querySelector(`.listcommand:nth-child(${number}) button .arrow i`).className = "fas fa-angle-right"
+        document.querySelector(`.listcommand:nth-child(${number}) button .arrow i`).style.paddingLeft = '6.5px'
+        document.querySelector(`.listcommand:nth-child(${number}) button .arrow i`).style.paddingRight = '6.5px'
     }
 }
