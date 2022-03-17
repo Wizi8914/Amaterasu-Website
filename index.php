20b1c756cc717937745de6cdb88af1c6bc5c0c3a
Wizi8914 <loick.konieczny@gmail.com>
2022-03-17 17:30:59 +0100

    change

--- index.php
+++ index.php
@@ -162,14 +162,11 @@
                 </nav>
             </div>
 
-            <footer class="sitefooter">
-                <span>Copyright by Wizi © 2019 - 2022</span>
-                <div class="linkfooter">
-                    <a href="https://discord.gg/5Mww29D9kB" class="footerlink" target="_blank"><i class="fab fa-discord"></i></a>
-                    <a href="https://github.com/Wizi8914/Amaterasu" class="footerlink" target="_blank"><i class="fab fa-github"></i></a>
-                    <a href="https://twitter.com/Wizi20540539" class="footerlink" target="_blank"><i class="fab fa-twitter"></i></a>
-                </div>
-            </footer>
+            <?php 
+                include "./Includes/footer.html"
+            ?>
         </div>
     </body>
 </html>
+
+
