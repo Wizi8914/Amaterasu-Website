ae45db309aca2ce3be69454a7046c0590b6357dc
Wizi8914 <loick.konieczny@gmail.com>
2022-03-17 17:45:12 +0100

    optimisation

--- pages/moderation.php
+++ pages/moderation.php
@@ -1,19 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <meta charset="UTF-8">
-        <title>Amaterasu support</title>
-        <link rel="shortcut icon" href="../assets/logo amaterasu.ico" type="image/x-icon">
-
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/5991870b6e.js" crossorigin="anonymous"></script>
-
-        <link rel="stylesheet" href="../css/style.css">
-        <link rel="stylesheet" href="../css/header.css">
-        <link rel="stylesheet" href="../css/commandlist.css">
-        <link rel="stylesheet" href="../css/loader.css">
-        <link rel="stylesheet" href="../css/footer.css">
-        <script src="../js/app.js"></script>
+        <?php
+            include "../Includes/link.html"
+        ?>
     </head>
 
     <body onload="load()">
