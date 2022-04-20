05072b6b3e150a84ffc5c37ee4ad3eccdda24cba
Wizi8914 <loick.konieczny@gmail.com>
2022-04-20 09:23:11 +0200

    change text in index

--- index.php
+++ index.php
@@ -52,13 +52,15 @@
                     <h1 class="title">A powerful multi-function Discord bot</h1>
                     <div class="mainbutton">
                         <div class="addbot">
-                            <a href="https://discord.com/oauth2/authorize?client_id=795298036644315166&scope=bot+applications.commands+identify+guilds&response_type=code&permissions=2080374975" target="_blank"><i class="fab fa-discord"></i> Ajouter a discord</a>
+                            <a href="https://discord.com/oauth2/authorize?client_id=795298036644315166&scope=bot+applications.commands+identify+guilds&response_type=code&permissions=2080374975" target="_blank"><i class="fab fa-discord"></i> Add to Discord</a>
                         </div>
                         <div class="doc">
                             <a href="./pages/divers.php" target="_blank"><i class="fa solid fa-list-ul"></i> Documentation</a>
                             
                         </div>
-                    </div>                    
+                    </div>
+                    
+                    <p class="paragraph">Amaterasu is a project currently led by a single person. It started to be developed in 2019, it is developed in Javascript more precisely in Node.js and uses the Discord.js library, Amaterasu is an open source project and evolving in time, there are currently several bugs present on the bot please excuse the developer. Many future additions are already planned. Currently Amaterasu has 7 command categories. A support server discord is available to report any bug or to have more information about the project. The website also has a precise documentation of each command. This system is also present on the bot with the help command.</p>
                 </nav>
             </div>
             <div class="fav">
@@ -67,15 +69,15 @@
                         <img src="./assets/img/feature/feature_1.png" class="left">
                     </div>
                     <div style="width: 100%;">
-                        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, corporis totam. Doloribus quasi consequuntur itaque?</p>
+                        <h2>Various and varied commands</h2>
+                        <p class="text">Find a great category of practical and more or less useful commands</p>
                     </div>
                 </div>
 
                 <div class="feature">
                     <div style="width: 100%;">
-                        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, corporis totam. Doloribus quasi consequuntur itaque?</p>
+                        <h2>A system for playing music</h2>
+                        <p class="text">Play your favorite music in a living room with your friends. With the ability to directly set a playlist, pause the music, or retrieve the lyrics of the current music.</p>
                     </div>
                     <div>
                         <img src="./assets/img/feature/feature_2.png" class="right">
@@ -87,15 +89,15 @@
                         <img src="./assets/img/feature/feature_3.png" class="left">
                     </div>
                     <div style="width: 100%;">
-                        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, corporis totam. Doloribus quasi consequuntur itaque?</p>
+                        <h2>Manage your server very simply</h2>
+                        <p class="text">Get information about your discord server and several other commands to manage your discord server</p>
                     </div>
                 </div>
 
                 <div class="feature">
                     <div style="width: 100%;">
-                        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, corporis totam. Doloribus quasi consequuntur itaque?</p>
+                        <h2>Discover anime related commands</h2>
+                        <p class="text">Use commands to get accurate information about your favorite anime and manga</p>
                     </div>
                     <div>
                         <img src="./assets/img/feature/feature_4.png" class="right">
@@ -103,10 +105,12 @@
                 </div>
 
                 <div class="feature">
-                    <div></div>
+                    <div>
+                        <img src="./assets/img/feature/feature_5.png" class="left">
+                    </div>
                     <div style="width: 100%;">
-                        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-                        <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, corporis totam. Doloribus quasi consequuntur itaque?</p>
+                        <h2>Interact with other members</h2>
+                        <p class="text">Discover a list of commands for the interaction between the members of the discord</p>
                     </div>
                 </div>
             </div>
